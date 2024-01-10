230adec6282ce7669384580b43afb50b1b83b9fe
seaweed78 <adhikarikshitiz12@gmail.com>
2024-01-10 15:18:12 +0545

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,89 @@
 import { useState } from "react";
 
 export default function App() {
-  return <Tip />;
-}
-
-function Tip() {
   const [bill, setBill] = useState(0);
   const [selfLike, setSelfLike] = useState(0);
   const [friendLike, setFriendLike] = useState(0);
 
-  let selfTip = Number((Number(selfLike) / 100) * bill);
-  let friendTip = Number((Number(friendLike) / 100) * bill);
-  let totalBill = Number(Number(bill) + (selfTip + friendTip) / 2);
+  function handleBillInput(amt) {
+    setBill(amt);
+  }
+
+  function handleSelfLike(likePercent) {
+    setSelfLike(likePercent);
+  }
+
+  function handleFriendLike(likePercent) {
+    setFriendLike(likePercent);
+  }
 
   function handleReset() {
     setBill(0);
     setSelfLike(0);
     setFriendLike(0);
   }
+
+  return (
+    <div className="app">
+      <div className="box">
+        <div className="comp bill">
+          <Bill bill={bill} onBillInput={handleBillInput} />
+        </div>
+        <div className="comp percent">
+          <p>How did you like the service?</p>
+          <TipPercentage likeBy={selfLike} onLike={handleSelfLike} />
+        </div>
+        <div className="comp percent">
+          <p>How did your friend like the service?</p>
+          <TipPercentage likeBy={friendLike} onLike={handleFriendLike} />
+        </div>
+        <div className="comp output">
+          <Output bill={bill} selfLike={selfLike} friendLike={friendLike} />
+        </div>
+
+        <div>
+          <button className="btn" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Bill({ bill, onBillInput }) {
   return (
     <div>
-      <p>
-        How much was the bill?{" "}
-        <input value={bill} onChange={(e) => setBill(e.target.value)}></input>
-      </p>
+      <p>How much was the bill ? </p>
+      <input
+        value={bill}
+        onChange={(e) => onBillInput(Number(e.target.value))}
+      ></input>
+    </div>
+  );
+}
 
-      <p>
-        How did you like the service?{" "}
-        <select value={selfLike} onChange={(e) => setSelfLike(e.target.value)}>
-          <option value={0}>Dissatisfied (0%)</option>
-          <option value={5}>It was okay (5%)</option>
-          <option value={10}>It was good (10%)</option>
-          <option value={20}>Absolutely Amazing! (20%)</option>
-        </select>
-      </p>
+function TipPercentage({ likeBy, onLike }) {
+  return (
+    <div>
+      <select value={likeBy} onChange={(e) => onLike(Number(e.target.value))}>
+        <option value={0}>Dissatisfied(0%)</option>
+        <option value={5}>It was okay(5%)</option>
+        <option value={10}>It was good(10%)</option>
+        <option value={20}>Absolutely Amazing!(20%)</option>
+      </select>
+    </div>
+  );
+}
 
-      <p>
-        How did your friend like the sevice?{" "}
-        <select
-          value={friendLike}
-          onChange={(e) => setFriendLike(e.target.value)}
-        >
-          <option value={0}>Dissatisfied (0%)</option>
-          <option value={5}>It was okay (5%)</option>
-          <option value={10}>It was good (10%)</option>
-          <option value={20}>Absolutely Amazing! (20%)</option>
-        </select>
-      </p>
-
-      <b>
-        You pay ${totalBill} (${selfTip} + ${friendTip}) tip
-      </b>
-
-      <div>
-        <button onClick={handleReset}>Reset</button>
-      </div>
+function Output({ bill, selfLike, friendLike }) {
+  let tip = (Number(selfLike + friendLike) / 2 / 100) * bill;
+  let totalAmt = bill + tip;
+  return (
+    <div>
+      <strong>
+        You pay ${totalAmt} (${bill} + ${tip} tip)
+      </strong>
     </div>
   );
 }
